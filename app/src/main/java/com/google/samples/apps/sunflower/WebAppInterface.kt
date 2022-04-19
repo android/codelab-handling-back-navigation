44f6729674f9fa89d3bc12cc0c8759dae284a596
Diego Zuluaga <diegozuluaga@google.com>
2022-04-18 19:36:30 -0700

    Replace Google Forms with an HTML web app
    Change-Id: Ib477d6d2ce5be9aa906a47f1e9d60cdf7171140d

---
+++ app/src/main/java/com/google/samples/apps/sunflower/WebAppInterface.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower
+
+import android.content.Context
+import android.webkit.JavascriptInterface
+import android.widget.Toast
+
+/** Instantiate the interface and set the context  */
+class WebAppInterface(private val mContext: Context) {
+
+    /** Show a toast from the web page  */
+    @JavascriptInterface
+    fun showToast(toast: String) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_LONG).show()
+    }
+}
