416314dd7332c67bdab81753b4517fc357572f09
Diego Zuluaga <diegozuluaga@google.com>
2022-04-24 19:36:14 -0700

    Enable edge-to-edge
    Change-Id: Ib021fa17d983ca8bc95c83285ddbf5d974742ffb

--- app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.kt
@@ -18,6 +18,7 @@ package com.google.samples.apps.sunflower
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.databinding.DataBindingUtil.setContentView
 import com.google.samples.apps.sunflower.databinding.ActivityGardenBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,5 +29,6 @@ class GardenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView<ActivityGardenBinding>(this, R.layout.activity_garden)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
--- app/src/main/java/com/google/samples/apps/sunflower/SurveyFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/SurveyFragment.kt
@@ -22,7 +22,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.OnBackPressedCallback
